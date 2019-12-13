bc3edcf4acad226253ff71a25a340f6ca6b59581
dsietziapp <dsietz@iapp.org>
2019-12-13 15:06:32 -0500

    working on #1

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
 
-extern crate log;
 extern crate env_logger;
+extern crate futures;
+extern crate log;
+extern crate pbd;
+#[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde_json;
 
 use log::*;
 use std::env;
+use std::time::{SystemTime};
 
+#[macro_use] pub mod macros;
 pub mod config;
+pub mod doc;
