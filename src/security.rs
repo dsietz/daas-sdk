38a3163f0bcb37af3c3e01c6247eab0d81ff46cd
dsietz <davidsietz@yahoo.com>
2020-01-25 11:45:02 -0500

    working on #10

--- src/security.rs
+++ src/security.rs
@@ -3,7 +3,7 @@
 //! These security features can be implemented in two manners:
 //! 
 //! 1. Instantiating a `DaaSGuard` object and calling it's methods
-//! 2. Implementing the `DaaSSecurityGaurd` traits for your own defined structure
+//! 2. Implementing the `DaaSSecurityGuard` traits for your own defined structure
 //!
 //! # Examples
 //!
@@ -12,7 +12,7 @@
 //! ```
 //! extern crate daas;
 //!
-//! use daas::security::{DaaSGuard, DaaSSecurityGaurd};
+//! use daas::security::{DaaSGuard, DaaSSecurityGuard};
 //!
 //! fn main() {
 //!     let guard = DaaSGuard {};
@@ -21,12 +21,12 @@
 //! }
 //! ```
 //! 
-//! //! Implementing the DaaSSecurityGaurd trait to generate a RSA keypair
+//! Implementing the DaaSSecurityGuard trait to generate a RSA keypair
 //!
 //! ```
 //! extern crate daas;
 //!
-//! use daas::security::{DaaSSecurityGaurd};
+//! use daas::security::{DaaSSecurityGuard};
 //!
 //! fn main() {
 //!     struct MyStruct {}
@@ -35,13 +35,114 @@
 //!             "Hello World!".to_string()
 //!         }
 //!     }
-//!     impl DaaSSecurityGaurd for MyStruct {}
+//!     impl DaaSSecurityGuard for MyStruct {}
 //! 
 //!     let my_obj = MyStruct {};
 //!     let keypair = my_obj.generate_keypair();
 //! 
 //!     println!("{}", my_obj.hello());
 //!     assert!(keypair.is_ok());    
+//! }
+//! ```
+//! 
+//! Following the steps to send data from source to target in a secured manner
+//!
+//! 1. Generate a symmetric key for use with another algorithm using AES
+//! 2. Encrypt the data using the AES symmetric key
+//! 3. Then you RSA-encrypt the symmetric key and transmit that
+//! 
+//! ```
+//! extern crate daas;
+//! extern crate openssl;
+//! 
+//! use daas::security::{DaaSGuard, DaaSSecurityGuard};
+//! use openssl::rsa::{Padding};
+//! use std::io;
+//! use std::io::prelude::*;
+//! use std::fs::File;
+//! 
+//! fn get_priv_pem() -> Vec<u8> {
+//!     let mut f = File::open("./tests/keys/priv-key.pem").unwrap();
+//!     let mut priv_pem = Vec::new();
+//!     f.read_to_end(&mut priv_pem).unwrap();
+//!     
+//!     priv_pem
+//! }
+//! 
+//! fn get_pub_pem() -> Vec<u8> {
+//!     let mut f = File::open("./tests/keys/pub-key.pem").unwrap();
+//!     let mut pub_pem = Vec::new();
+//!     f.read_to_end(&mut pub_pem).unwrap();
+//!     
+//!     pub_pem
+//! }
+//!
+//! fn main() {
+//!     let priv_key = get_priv_pem();
+//!     let pub_key = get_pub_pem();
+//!     let guard = DaaSGuard {};
+//!     let key = guard.generate_symmetric_key();
+//!     let nonce = guard.generate_nonce();
+//!     let padding = Padding::PKCS1;
+//!     let mut f = File::open("./tests/example_audio_clip.mp3").unwrap();
+//!     let mut mp3 = Vec::new();
+//!     f.read_to_end(&mut mp3).unwrap();
+//!     
+//!     // 1. encrypt the mps data using the symmetric key
+//!     let encrypted_data = match guard.encrypt_data(key.clone(), Some(&nonce.clone()), mp3.clone()) {
+//!         Ok(msg) => {
+//!             assert!(true);
+//!             msg                
+//!         },
+//!         Err(err) => {
+//!             assert!(false);
+//!             panic!("{:?}", err);
+//!         },
+//!     };    
+//!     
+//!     // 2. Encrypt the symmetric key
+//!     let encrypted_key = match guard.encrypt_symmetric_key(pub_key, key.clone(), padding) {
+//!         Ok(e_key) => {
+//!             assert_eq!(e_key.len(), 256);
+//!             e_key
+//!         },
+//!         Err(err) => {
+//!             assert!(false);
+//!             panic!("{:?}", err);
+//!         },
+//!     };
+//!     
+//!     // The data source sends the following items to the recipient: 
+//!     // + padding
+//!     // + nonce
+//!     // + encrypted symmetric key
+//!     // + encrypted mps data
+//!     
+//!     // 3. Decrypt the symmetric key
+//!     let decrypted_key = match guard.decrypt_symmetric_key(priv_key, encrypted_key, padding) {
+//!         Ok(e_key) => {
+//!             assert_eq!(e_key.len(), 16);
+//!             e_key
+//!         },
+//!         Err(err) => {
+//!             assert!(false);
+//!             panic!("{:?}", err);
+//!         },
+//!     };
+//!     
+//!     // 4. Decrypt the data using the symmetric key
+//!     let decrypted_data = match guard.decrypt_data(key, Some(&nonce), encrypted_data) {
+//!         Ok(msg) => {
+//!             assert!(true);
+//!             msg                
+//!         },
+//!         Err(err) => {
+//!             assert!(false);
+//!             panic!("{:?}", err);
+//!         },
+//!     }; 
+//!     
+//!     assert_eq!(mp3, decrypted_data);
 //! }
 //! ```
 use super::*;
@@ -51,20 +152,10 @@ use openssl::rsa::{Rsa, Padding};
 use openssl::symm::{decrypt, encrypt, Cipher};
 use rand::Rng; 
 use rand::distributions::Alphanumeric;
-/*
-Due to the mathematics (and padding) behind RSA encryption, you can only encrypt very small values.
-In order to use RSA encryption with larger values, typically you generate a symmetric key for use with another algorithm, such as AES. 
-Then you encrypt the data using the AES symmetric key (there is no limitation on size using a symmetric encryption algorithm) and then you RSA-encrypt 
-just the symmetric key and transmit that. AES keys are 16-32 bytes in size so they can easily fit within the RSA-encryption limitations.
-Then the recipient decrypts the symmetric key using their private RSA key and then they decrypt the encrypted data using the decrypted symmetric key.
-RSA encryption is also much slower than AES encryption, so this yields better performance anyway.
 
-SEE ALSO: https://docs.rs/openssl/0.10.26/openssl/aes/index.html
-*/
-
-
-// priv_key = the priuvate key as pem
-pub trait DaaSSecurityGaurd{
+/// Trait that provides the DaaS security functionality 
+pub trait DaaSSecurityGuard{
+    /// Generates a RSA (private/public) keypair
     fn generate_keypair(&self) -> Result<(Vec<u8>,Vec<u8>,usize),DaaSSecurityError>{
         let rsa = Rsa::generate(2048).unwrap();
         let priv_key: Vec<u8> = rsa.private_key_to_pem().unwrap();
@@ -73,6 +164,7 @@ pub trait DaaSSecurityGaurd{
         Ok((priv_key, pub_key, rsa.size() as usize))
     }
 
+    /// Generates a random alphanumeric key with a length of 16 characters
     fn generate_symmetric_key(&self) -> Vec<u8>{
         rand::thread_rng()
             .sample_iter(&Alphanumeric)
@@ -82,6 +174,7 @@ pub trait DaaSSecurityGaurd{
             .to_vec()
     }
 
+    /// Generates a random alphanumeric nonce (a.k.a. IV) with a length of 16 characters
     fn generate_nonce(&self) -> Vec<u8>{
         rand::thread_rng()
             .sample_iter(&Alphanumeric)
@@ -91,6 +184,7 @@ pub trait DaaSSecurityGaurd{
             .to_vec()
     }
 
+    /// Decrypts the data (small or large) using the symmetric key, IV and AES encryption algorithm
     fn decrypt_data(&self, key: Vec<u8>, nonce: Option<&[u8]>, data_to_decrypt: Vec<u8>) -> Result<Vec<u8>, DaaSSecurityError> {
         match decrypt(Cipher::aes_128_cbc(), &key, nonce, &data_to_decrypt) {
             Ok(data) => {
@@ -103,6 +197,7 @@ pub trait DaaSSecurityGaurd{
         }
     }
 
+    /// Encrypts the data (small or large) using the symmetric key, IV and AES encryption algorithm
     fn encrypt_data(&self, key: Vec<u8>, nonce: Option<&[u8]>, data_to_encrypt: Vec<u8>) -> Result<Vec<u8>, DaaSSecurityError> {
         match encrypt(Cipher::aes_128_cbc(), &key, nonce, &data_to_encrypt) {
             Ok(cipherdata) => {
@@ -115,6 +210,30 @@ pub trait DaaSSecurityGaurd{
         }
     }
 
+    /// Decrypts the symmetric key using RSA algorithm for the specified padding
+    fn decrypt_symmetric_key(&self, priv_key: Vec<u8>, encrypted_key: Vec<u8>, padding: Padding) -> Result<Vec<u8>,DaaSSecurityError> {
+        let receiver = match Rsa::private_key_from_pem(&priv_key) {
+            Ok(rsa) => rsa,
+            Err(err) => {
+                debug!("{}", err);
+                return Err(DaaSSecurityError::BadKeyPairError);
+            },
+        };
+        //let sz = std::cmp::max(encrypted_data.len() as usize, priv_key.len() as usize);
+        let mut message: Vec<u8> = vec![0; encrypted_key.len()];
+        
+        match receiver.private_decrypt(&encrypted_key, message.as_mut_slice(), padding){
+            Ok(_sz) => {
+                Ok(self.clean_decrypted(message))
+            },
+            Err(err) => {
+                debug!("{}", err);
+                return Err(DaaSSecurityError::DecryptionError);
+            },
+        }
+    }
+
+    /// Encrypts the symmetric key using RSA algorithm for the specified padding
     fn encrypt_symmetric_key(&self, pub_key: Vec<u8>, key_to_encrypt: Vec<u8>, padding: Padding) -> Result<Vec<u8>,DaaSSecurityError> {
         let sender = match Rsa::public_key_from_pem(&pub_key){
             Ok(rsa) => rsa,
@@ -128,29 +247,8 @@ pub trait DaaSSecurityGaurd{
 
         Ok(encrypted_data)
     }
-   
-    fn decrypt_symmetric_key(&self, priv_key: Vec<u8>, encrypted_data: Vec<u8>, padding: Padding) -> Result<Vec<u8>,DaaSSecurityError> {
-        let receiver = match Rsa::private_key_from_pem(&priv_key) {
-            Ok(rsa) => rsa,
-            Err(err) => {
-                debug!("{}", err);
-                return Err(DaaSSecurityError::BadKeyPairError);
-            },
-        };
-        //let sz = std::cmp::max(encrypted_data.len() as usize, priv_key.len() as usize);
-        let mut message: Vec<u8> = vec![0; encrypted_data.len()];
-        
-        match receiver.private_decrypt(&encrypted_data, message.as_mut_slice(), padding){
-            Ok(_sz) => {
-                Ok(self.clean_decrypted(message))
-            },
-            Err(err) => {
-                debug!("{}", err);
-                return Err(DaaSSecurityError::DecryptionError);
-            },
-        }
-    }
 
+    /// Removes the control NUL characters form the decrypted message
     fn clean_decrypted(&self, mut message: Vec<u8>) -> Vec<u8> {
         //remove the control NUL characters
         let zero: u8 = 0;
@@ -170,8 +268,9 @@ pub trait DaaSSecurityGaurd{
     }
 }
 
+/// Represents the DaaS Security Gaurd
 pub struct DaaSGuard {}
-impl DaaSSecurityGaurd for DaaSGuard{}
+impl DaaSSecurityGuard for DaaSGuard{}
 
 #[cfg(test)]
 mod tests {
@@ -251,6 +350,75 @@ mod tests {
                 assert!(false);
             },
         }
+    }
+
+    #[test]
+    fn test_happy_path_mp3() {
+        let priv_key = get_priv_pem();
+        let pub_key = get_pub_pem();
+        let guard = DaaSGuard {};
+        let key = guard.generate_symmetric_key();
+        let nonce = guard.generate_nonce();
+        let padding = Padding::PKCS1;
+        let mut f = File::open("./tests/example_audio_clip.mp3").unwrap();
+        let mut mp3 = Vec::new();
+        f.read_to_end(&mut mp3).unwrap();
+
+        // 1. encrypt the mps data using the symmetric key
+        let encrypted_data = match guard.encrypt_data(key.clone(), Some(&nonce.clone()), mp3.clone()) {
+            Ok(msg) => {
+                assert!(true);
+                msg                
+            },
+            Err(err) => {
+                assert!(false);
+                panic!("{:?}", err);
+            },
+        };    
+
+        // 2. Encrypt the symmetric key
+        let encrypted_key = match guard.encrypt_symmetric_key(pub_key, key.clone(), padding) {
+            Ok(e_key) => {
+                assert_eq!(e_key.len(), 256);
+                e_key
+            },
+            Err(err) => {
+                assert!(false);
+                panic!("{:?}", err);
+            },
+        };
+
+        // The data source sends the following items to the recipient: 
+        // + padding
+        // + nonce
+        // + encrypted symmetric key
+        // + encrypted mps data
+
+        // 3. Decrypt the symmetric key
+        let decrypted_key = match guard.decrypt_symmetric_key(priv_key, encrypted_key, padding) {
+            Ok(e_key) => {
+                assert_eq!(e_key.len(), 16);
+                e_key
+            },
+            Err(err) => {
+                assert!(false);
+                panic!("{:?}", err);
+            },
+        };
+
+        // 4. Decrypt the data using the symmetric key
+        let decrypted_data = match guard.decrypt_data(key, Some(&nonce), encrypted_data) {
+            Ok(msg) => {
+                assert!(true);
+                msg                
+            },
+            Err(err) => {
+                assert!(false);
+                panic!("{:?}", err);
+            },
+        }; 
+
+        assert_eq!(mp3, decrypted_data);
     }
 
     #[test]
